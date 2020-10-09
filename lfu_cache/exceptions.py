8ee25df79e381ce00bcdc4e2786947c49d166485
Ritesh Nadhani <riteshn@gmail.com>
2020-10-09 16:13:48 -0700

    initial commit
    A new package implemeting O(1) based LFU cache eviction scheme. The
    implementation uses linkedin list so this is an attempt to first design
    one as I didnt find any good implementation.
    
    The list implementation is heavily inspired by golang container/list
    verison with similar API call. It implements a ring based double linked
    list with tests mimicking original golang modules.
    
    Primary use of this library is to use the lfu_cache but one could use
    just the linked list too.
    
    Thread safety has not been tested.

---
+++ lfu_cache/exceptions.py
@@ -0,0 +1,15 @@
+class ListException(Exception):
+    pass
+
+
+class OtherListElement(ListException):
+    """Exception raised when you try to work with element of another list"""
+
+    def __init__(self, l, element):
+        self.l = l
+        self.element = element
+
+
+    def __str__(self):
+        return f"{self.element} not an element of {self.l}"
+
