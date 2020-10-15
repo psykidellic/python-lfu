3e720752a347586828ab98e8407d155a6ea1c03f
Ritesh Nadhani <riteshn@gmail.com>
2020-10-14 21:09:43 -0700

    package management and dev setup
    setuptools based setup.py
    tox.ini for testing
    
    To setup your dev, install pyenv and latest 3.6, 3.7, 3.8. I use by
    default 3.6 and python_lfu virtualenv based on 3.6.
    
    Then its a matter of pyenv local python_lfu 3.6 3.7 3.8.
    
    This blog worked for me: https://www.loganasherjones.com/2018/03/setting-up-your-python-environment/

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import re
+import os
+import codecs
+
+name = 'python-lfu'
+package = 'lfu_cache'
+description = 'A Python LFU cache implementation with O(1) eviction scheme'
+url = 'https://github.com/psykidellic/python-lfu'
+author = 'Ritesh Nadhani'
+author_email = 'riteshn@gmail.com'
+license = 'Unilicense'
+install_requires = []
+extras_require = {}
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = codecs.open(os.path.join(package, '__init__.py'), encoding='utf-8').read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+EXCLUDE_FROM_PACKAGES = []
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    long_description=description,
+    author=author,
+    author_email=author_email,
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires='>=3.6',
+    classifiers=classifiers,
+)
